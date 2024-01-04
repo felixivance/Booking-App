b9eb699ea327c822c3a71a1cc784abb5bf12fde9
Felix <felixrunye@gmail.com>
2024-01-04 16:01:25 +0300

    saving data to the db

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -1,5 +1,5 @@
 import dbConnect from "@/backend/config/dbConnect";
-import { allRooms } from "@/backend/controllers/roomController";
+import { allRooms, newRoom } from "@/backend/controllers/roomController";
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
 
@@ -15,6 +15,12 @@ dbConnect();
 
 router.get(allRooms);
 
+router.post(newRoom);
+
 export async function GET( request: NextRequest , context: RequestContext) {
+    return router.run(request, context);
+}
+
+export async function POST( request: NextRequest , context: RequestContext) {
     return router.run(request, context);
 }
--- backend/controllers/roomController.ts
+++ backend/controllers/roomController.ts
@@ -1,6 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
 
 export const allRooms = async (request: NextRequest) => {
     return NextResponse.json({"message": "Hello from the API!"})
+}
+
+export const newRoom = async (request: NextRequest) => {
+    const body = await request.json();
+
+    const room = await Room.create(body);
+
+    return NextResponse.json({
+        success: true,
+        room
+    })
 }
