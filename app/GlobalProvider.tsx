07e49a8eb0537f0948e66bb4959bfc2a42e07e27
Felix <felixrunye@gmail.com>
2024-01-05 16:27:04 +0300

    updated providers

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,4 +1,15 @@
+import { Toaster } from 'react-hot-toast';
 
 export function GlobalProvider({children} : {children: React.ReactNode}) {
-    return <>{children} </>;
+    return <>
+        <Toaster 
+            position="top-center"
+            toastOptions={{
+                style: {
+                    fontSize: "14px"
+                }
+            }}
+        />
+        {children} 
+        </>;
 }
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import React from 'react'
 import RoomItem from './room/roomItem'
+import toast from 'react-hot-toast';
 
 type Props = {}
 
 const Home = (props: Props) => {
+
+    
+
   return (
         <div>
         <section id="rooms" className="container mt-5">
-            <h2 className="mb-3 ml-2 stays-heading">All Rooms</h2>
+            <h2 className="mb-3 ml-2 stays-heading" onClick={()=>toast.success('Hello toast!')} >All Rooms</h2>
             <a href="/search" className="ml-2 back-to-search">
             <i className="fa fa-arrow-left"></i> Back to Search
             </a>
