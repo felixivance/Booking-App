6cf2f42a78548ccd7426307aaf6e139dd934c114
Felix <felixrunye@gmail.com>
2024-01-15 12:56:16 +0300

    created a loader

---
+++ components/layout/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Loader = (props: Props) => {
+  return (
+    <div className="d-flex justify-content-center">Loader</div>
+  )
+}
+
+export default Loader
